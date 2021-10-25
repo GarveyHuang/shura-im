60afe309944bdd73a2b73ca56d0a50b8048cd965
GarveyHuang <ishuangjw@foxmail.com>
2021-10-25 18:26:41 +0800

    常量类封装

---
+++ im-gateway/src/main/java/com/shura/im/gateway/constant/Constants.java
@@ -0,0 +1,24 @@
+package com.shura.im.gateway.constant;
+
+/**
+ * @Author: Garvey
+ * @Created: 2021/10/23
+ * @Description: 常量类
+ */
+public final class Constants {
+
+    /**
+     * 账号前缀
+     */
+    public final static String ACCOUNT_PREFIX = "im-account:";
+
+    /**
+     * 路由信息前缀
+     */
+    public final static String ROUTE_PREFIX = "im-route:";
+
+    /**
+     * 登录状态前缀
+     */
+    public final static String LOGIN_STATUS_PREFIX = "login-status";
+}
