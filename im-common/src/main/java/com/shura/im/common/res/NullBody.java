b2244bc6d5cb1226530306f00c9bf6c4b640427b
GarveyHuang <ishuangjw@foxmail.com>
2021-10-25 18:22:42 +0800

    空返回体对象封装

---
+++ im-common/src/main/java/com/shura/im/common/res/NullBody.java
@@ -0,0 +1,15 @@
+package com.shura.im.common.res;
+
+/**
+ * @Author: Garvey
+ * @Created: 2021/10/23
+ * @Description: 空对象，用在泛型中，表示没有额外的请求参数或者返回参数
+ */
+public class NullBody {
+
+    public NullBody() {}
+
+    public static NullBody create() {
+        return new NullBody();
+    }
+}
