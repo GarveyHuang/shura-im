3e52d827d8c95ce87266afd1cf13b1e82977e1a5
GarveyHuang <ishuangjw@foxmail.com>
2021-10-28 22:26:08 +0800

    添加服务监听线程

--- im-gateway/src/main/java/com/shura/im/gateway/GatewayApplication.java
+++ im-gateway/src/main/java/com/shura/im/gateway/GatewayApplication.java
@@ -1,7 +1,9 @@
 package com.shura.im.gateway;
 
+import com.shura.im.gateway.kit.ServerListListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,12 +13,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Description:
  */
 @SpringBootApplication
-public class GatewayApplication  {
+public class GatewayApplication implements CommandLineRunner {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GatewayApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
         LOGGER.info("Start im route success!!!");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // 监听服务
+        Thread thread = new Thread(new ServerListListener());
+        thread.setName("zk-listener");
+        thread.start();
     }
 }
