bc768571e314f8b2c0325b24a3f3e42f79609a43
GarveyHuang <ishuangjw@foxmail.com>
2021-11-24 22:39:24 +0800

    创建消息轮询任务

--- im-client/src/main/java/com/shura/im/client/IMClientApplication.java
+++ im-client/src/main/java/com/shura/im/client/IMClientApplication.java
@@ -1,7 +1,11 @@
 package com.shura.im.client;
 
+import com.shura.im.client.component.ClientInfo;
+import com.shura.im.client.scanner.Scan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,12 +15,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Description:
  */
 @SpringBootApplication
-public class IMClientApplication {
+public class IMClientApplication implements CommandLineRunner {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(IMClientApplication.class);
+
+    @Autowired
+    private ClientInfo clientInfo;
 
     public static void main(String[] args) {
         SpringApplication.run(IMClientApplication.class, args);
         LOGGER.info("Start client success!!!");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Scan scan = new Scan();
+        Thread thread = new Thread(scan);
+        thread.setName("scan-thread");
+        thread.start();
+        clientInfo.saveStartDate();
     }
 }
